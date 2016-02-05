fb7583627bc33830883bab1059a9915442150626
taihangy <yetaihang.zju@gmail.com>
2016-02-05 10:57:00 -0500

    camera function done

--- Hybrid Mobile App Development Frameworks/ionic/conFusion/www/js/controllers.js
+++ Hybrid Mobile App Development Frameworks/ionic/conFusion/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('conFusion.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage, $ionicPlatform, $cordovaCamera) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -11,6 +11,7 @@ angular.module('conFusion.controllers', [])
 
   // Form data for the login modal
   $scope.loginData = $localStorage.getObject('userinfo', '{}');
+  $scope.registration = {};
   $scope.reservation = {};
 
   // Create the login modal that we will use later
@@ -40,6 +41,58 @@ angular.module('conFusion.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
+
+  // Create the registration modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/register.html', {
+      scope: $scope
+  }).then(function (modal) {
+      $scope.registerform = modal;
+  });
+
+  // Triggered in the registration modal to close it
+  $scope.closeRegister = function () {
+      $scope.registerform.hide();
+  };
+
+  // Open the registration modal
+  $scope.register = function () {
+      $scope.registerform.show();
+  };
+
+  // Perform the registration action when the user submits the registration form
+  $scope.doRegister = function () {
+      console.log('Doing reservation', $scope.registration);
+
+      // Simulate a registration delay. Remove this and replace with your registration
+      // code if using a registration system
+      $timeout(function () {
+          $scope.closeRegister();
+      }, 1000);
+  };
+  
+  $ionicPlatform.ready(function() {
+      var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 100,
+        targetHeight: 100,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false
+      };
+       $scope.takePicture = function() {
+          $cordovaCamera.getPicture(options).then(function(imageData) {
+              $scope.registration.imgSrc = "data:image/jpeg;base64," + imageData;
+          }, function(err) {
+              console.log(err);
+          });
+
+          $scope.registerform.show();
+
+      };
+  });
 
   // Create the reservation modal that we will use later
   $ionicModal.fromTemplateUrl('templates/reserve.html', {
